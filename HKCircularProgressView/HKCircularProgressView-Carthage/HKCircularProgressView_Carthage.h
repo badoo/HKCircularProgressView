5fd28e6f756b7a995f72ae1e8c77da23cd20afba
alexander.balaban <alexander.balaban@corp.badoo.com>
2018-02-01 14:27:22 +0000

    shared framework for carthage

---
+++ HKCircularProgressView/HKCircularProgressView-Carthage/HKCircularProgressView_Carthage.h
@@ -0,0 +1,20 @@
+//
+//  HKCircularProgressView_Carthage.h
+//  HKCircularProgressView-Carthage
+//
+//  Created by alexander.balaban on 01/02/2018.
+//  Copyright © 2018 Panos Baroudjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HKCircularProgressView_Carthage.
+FOUNDATION_EXPORT double HKCircularProgressView_CarthageVersionNumber;
+
+//! Project version string for HKCircularProgressView_Carthage.
+FOUNDATION_EXPORT const unsigned char HKCircularProgressView_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HKCircularProgressView_Carthage/PublicHeader.h>
+#import <HKCircularProgressView_Carthage/HKCircularProgressLayer.h>
+#import <HKCircularProgressView_Carthage/HKCircularProgressView.h>
+
